60ab294969b90952b65b22e4244140b4550e3587
Vitalik Vakulenko <121180497+vakulenko10@users.noreply.github.com>
2025-04-16 00:45:36 +0200

    nodemailer added

---
+++ src/lib/mailer.ts
@@ -0,0 +1,26 @@
+import nodemailer from "nodemailer";
+
+export const transporter = nodemailer.createTransport({
+  service: "gmail",
+  auth: {
+    user: process.env.GMAIL_USER,
+    pass: process.env.GMAIL_PASS,
+  },
+});
+
+export async function sendEmail({
+  to,
+  subject,
+  html,
+}: {
+  to: string;
+  subject: string;
+  html: string;
+}) {
+  return transporter.sendMail({
+    from: process.env.EMAIL_FROM,
+    to,
+    subject,
+    html,
+  });
+}
