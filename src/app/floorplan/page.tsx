37faa10c2de4233cae514c546329593a177ae25a
Vitalik Vakulenko <121180497+vakulenko10@users.noreply.github.com>
2025-04-30 12:45:49 +0200

    removed floorplan route from our app

--- src/app/floorplan/page.tsx
+++
@@ -1,9 +0,0 @@
-import TableFloor from "@/components/TableFloor";
-
-export default function FloorPage() {
-  return (
-    <main className="p-8">
-      <TableFloor />
-    </main>
-  );
-}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,12 @@
 "use client";
 import { AdminLoginButton } from "@/components/AdminLoginButton";
+import TableFloor from "@/components/TableFloor";
 
 export default function Home() {
   return (
     <div className="p-2">
       <div className="">
+        <TableFloor />
         <AdminLoginButton />
       </div>
     </div>
